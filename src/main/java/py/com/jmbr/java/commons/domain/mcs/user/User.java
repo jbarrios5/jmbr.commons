1e7df1a4c8b38aa54f6195733cbb2ec78dabccb6
juan <marcecinco@fpuna.edu.py>
2023-11-21 17:14:46 -0300

    Modificacion de clases

--- src/main/java/py/com/jmbr/java/commons/domain/mcs/user/User.java
+++ src/main/java/py/com/jmbr/java/commons/domain/mcs/user/User.java
@@ -2,6 +2,7 @@ package py.com.jmbr.java.commons.domain.mcs.user;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,8 +14,10 @@ public class User {
 
     private Long id;
     private String name;
+    private String document;
     @JsonIgnore()
     private String password;
-    private String last_name;
+    @JsonProperty("last_name")
+    private String lastName;
     private Boolean status;
 }
