22ec6ab794a2ae8e438ad3b4e6ac3c9a206c3c67
juan <marcecinco@fpuna.edu.py>
2023-12-01 17:31:41 -0300

    validacion de access token

--- src/main/java/py/com/jmbr/java/commons/context/RequestContextInitializer.java
+++ src/main/java/py/com/jmbr/java/commons/context/RequestContextInitializer.java
@@ -2,11 +2,13 @@ package py.com.jmbr.java.commons.context;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
 import py.com.jmbr.java.commons.exception.JMBRException;
 import py.com.jmbr.java.commons.exception.JMBRExceptionType;
 import py.com.jmbr.java.commons.logger.RequestUtil;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class RequestContextInitializer implements HandlerInterceptor {
+    private static final String AUTH_URI = "http://localhost:8081/mcs.auth/auth/v1";
     private static final Logger logger = LoggerFactory.getLogger(RequestContextInitializer.class);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -32,12 +35,12 @@ public class RequestContextInitializer implements HandlerInterceptor {
                 logger.warn(RequestUtil.LOG_FORMATT,"no-log-id","apiKey is required",null);
                 throw new JMBRException("Error en autenticacion", JMBRExceptionType.FALTAL, HttpStatus.BAD_REQUEST);
              }
-
-
             return Boolean.TRUE;
-
-
         }
+        logger.info("cheking at={}",request.getHeader("access_token"));
+        if(!isAccessTokenValid(request.getHeader("access_token")))
+            throw new JMBRException("Error en autenticacion", JMBRExceptionType.FALTAL, HttpStatus.BAD_REQUEST);
+
 
         return Boolean.TRUE;
     }
@@ -50,5 +53,24 @@ public class RequestContextInitializer implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
+    }
+
+    private boolean isAccessTokenValid(String accessToken){
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders header = new HttpHeaders();
+        ResponseEntity<Boolean> response;
+        HttpEntity<?> entity = new HttpEntity<>(HttpEntity.EMPTY,header);
+        String URI = UriComponentsBuilder
+                .fromUriString(AUTH_URI.concat("/verify"))
+                .queryParam("access_token",accessToken)
+                .toUriString();
+        logger.info(RequestUtil.LOG_FORMATT,"no-log-id","isAccessTokenValid:request",entity.toString());
+        response = restTemplate.exchange(URI, HttpMethod.POST,entity,Boolean.class);
+        logger.info(RequestUtil.LOG_FORMATT,"no-log-id","isAccessTokenValid:response",response);
+        if(response.getBody() != null)
+            return response.getBody();
+        else
+            return false;
     }
 }
