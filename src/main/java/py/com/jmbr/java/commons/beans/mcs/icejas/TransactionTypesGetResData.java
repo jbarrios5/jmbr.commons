8b68fdfba9b316b5b097e954caeba430bfa4a2ff
juan barrios <jbarrios@roshka.com>
2023-12-07 17:35:26 -0300

    Se agrega envoltorio

--- src/main/java/py/com/jmbr/java/commons/beans/mcs/icejas/TransactionTypesGetResData.java
+++ src/main/java/py/com/jmbr/java/commons/beans/mcs/icejas/TransactionTypesGetResData.java
@@ -1,2 +1,11 @@
-package py.com.jmbr.java.commons.beans.mcs.icejas;public class TransactionTypesGetResData {
+package py.com.jmbr.java.commons.beans.mcs.icejas;
+
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import py.com.jmbr.java.commons.domain.mcs.icejas.TransactionTypesGetRes;
+@NoArgsConstructor
+@Setter
+public class TransactionTypesGetResData {
+
+    private TransactionTypesGetRes data;
 }
--- src/main/java/py/com/jmbr/java/commons/domain/mcs/icejas/TransactionTypesGetRes.java
+++ src/main/java/py/com/jmbr/java/commons/domain/mcs/icejas/TransactionTypesGetRes.java
@@ -1,7 +1,6 @@
 package py.com.jmbr.java.commons.domain.mcs.icejas;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
@@ -9,7 +8,7 @@ import java.util.List;
 
 @Setter
 @NoArgsConstructor
-public class TransactionTypeGetRes {
+public class TransactionTypesGetRes {
     @JsonProperty("transaction_types")
     List<TransactionType> transactionTypes;
 }
