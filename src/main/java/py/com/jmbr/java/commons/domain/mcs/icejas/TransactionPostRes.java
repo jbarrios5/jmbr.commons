2ea766c32b0144a27759ca61999b2d95add12fa4
juan <marcecinco@fpuna.edu.py>
2023-12-14 21:20:28 -0300

    Se corrige campos

--- src/main/java/py/com/jmbr/java/commons/domain/mcs/icejas/TransactionPostRes.java
+++ src/main/java/py/com/jmbr/java/commons/domain/mcs/icejas/TransactionPostRes.java
@@ -1,6 +1,5 @@
 package py.com.jmbr.java.commons.domain.mcs.icejas;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,7 +10,6 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class TransactionPostRes {
-    @JsonProperty("transaction_id")
     private Integer transactionId;
     private String message;
     private Integer status;
