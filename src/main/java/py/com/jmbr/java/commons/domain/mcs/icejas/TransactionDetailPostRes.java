2acb31d8b77b7ede0dfe8c132a3589c81421b617
juan <marcecinco@fpuna.edu.py>
2023-12-22 16:23:48 -0300

    Se agrega nueva propiedad para obtener los detalles de la transaccion

--- src/main/java/py/com/jmbr/java/commons/domain/mcs/icejas/TransactionDetailPostRes.java
+++ src/main/java/py/com/jmbr/java/commons/domain/mcs/icejas/TransactionDetailPostRes.java
@@ -3,15 +3,20 @@ package py.com.jmbr.java.commons.domain.mcs.icejas;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class TransactionDetailPostRes {
     private Integer transactionId;
     private BigDecimal amount;
+    private String transactionTypeName;
+    private String transactionCategory;
     private BigDecimal currentBalance;
+    private String transactionDetail;
     private String registerDate;
     private Integer userId;
 }
