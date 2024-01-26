18cd180e4c58fd02329d4e8a76b4ff15cc3b43cc
juan <marcecinco@fpuna.edu.py>
2024-01-25 21:40:40 -0300

    Se agrega clases para agrega nuevos tipos de transacciones

---
+++ src/main/java/py/com/jmbr/java/commons/beans/mcs/icejas/TransactionTypesPostRestData.java
@@ -0,0 +1,13 @@
+package py.com.jmbr.java.commons.beans.mcs.icejas;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import py.com.jmbr.java.commons.domain.mcs.icejas.TransactionTypesPostRes;
+@Getter
+@Setter
+@NoArgsConstructor
+public class TransactionTypesPostRestData {
+
+    private TransactionTypesPostRes data;
+}
---
+++ src/main/java/py/com/jmbr/java/commons/domain/mcs/icejas/TransactionTypesPostRes.java
@@ -0,0 +1,14 @@
+package py.com.jmbr.java.commons.domain.mcs.icejas;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class TransactionTypesPostRes {
+    private Boolean isInserted;
+}
