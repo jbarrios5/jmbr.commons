9e55b2caaad416ede1c9c3736eb4edcbaaba2911
juan <marcecinco@fpuna.edu.py>
2024-01-29 21:16:05 -0300

    Se agrega clases para agrega cierre del mes

---
+++ src/main/java/py/com/jmbr/java/commons/beans/mcs/icejas/TransactionMonthClosedPostResData.java
@@ -0,0 +1,17 @@
+package py.com.jmbr.java.commons.beans.mcs.icejas;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import py.com.jmbr.java.commons.domain.mcs.icejas.TransactionMonthClosedPostRes;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class TransactionMonthClosedPostResData {
+
+    private TransactionMonthClosedPostRes data;
+
+}
---
+++ src/main/java/py/com/jmbr/java/commons/domain/mcs/icejas/TransactionMonthClosedPostRes.java
@@ -0,0 +1,15 @@
+package py.com.jmbr.java.commons.domain.mcs.icejas;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class TransactionMonthClosedPostRes {
+    private Boolean isInserted;
+    private String message;
+}
