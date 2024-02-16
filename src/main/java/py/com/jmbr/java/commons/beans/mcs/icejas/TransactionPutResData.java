c30cf76811f910e9df323cb2d3f2c9edd5266add
juan barrios <jbarrios@roshka.com>
2024-02-16 11:48:02 -0300

    Se agrega objeto request para actualizacion de una transaccion

---
+++ src/main/java/py/com/jmbr/java/commons/beans/mcs/icejas/TransactionPutResData.java
@@ -0,0 +1,12 @@
+package py.com.jmbr.java.commons.beans.mcs.icejas;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import py.com.jmbr.java.commons.domain.mcs.icejas.TransactionPutRes;
+@Getter
+@Setter
+@NoArgsConstructor
+public class TransactionPutResData {
+    private TransactionPutRes data;
+}
---
+++ src/main/java/py/com/jmbr/java/commons/domain/mcs/icejas/TransactionPutRes.java
@@ -0,0 +1,13 @@
+package py.com.jmbr.java.commons.domain.mcs.icejas;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TransactionPutRes {
+    private String message;
+    private Boolean status;
+}
