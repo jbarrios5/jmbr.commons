7a11de445ebb3776c432b8e52869bafc6e2af795
juan barrios <jbarrios@roshka.com>
2024-02-20 11:49:56 -0300

    Se agrega clases para eliminar una transacion

---
+++ src/main/java/py/com/jmbr/java/commons/beans/mcs/icejas/TransactionDeleteResData.java
@@ -0,0 +1,15 @@
+package py.com.jmbr.java.commons.beans.mcs.icejas;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import py.com.jmbr.java.commons.domain.mcs.icejas.TransactionDeleteRes;
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class TransactionDeleteResData {
+    private TransactionDeleteRes data;
+}
+
